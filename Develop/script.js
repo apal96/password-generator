2488fa783949a9e5add7cac4389cf751f14d7bed
apal <alishapal96@gmail.com>
2021-06-23 19:05:53 -0700

    add if and else if statement for prompt

--- Develop/script.js
+++ Develop/script.js
@@ -15,12 +15,17 @@ generateBtn.addEventListener("click", writePassword);
 
 //generate password function
 function generatePassword(){
- var passLengthPrompt=prompt("How many characters should the password contain?\n(Please choose a length between 8 and 128 characters.) ");
- var specialCharacterConfirm = confirm("Click to confirm using special characters");
- var upperCaseConfirm = confirm("Click to confirm using upper case characters");
- var lowerCaseConfirm = confirm("Click to confirm using lower case characters");
-
-
-
-
+ var passLengthPrompt=prompt("How many characters should the password contain?) ");
+ console.log(passLengthPrompt);
+//  var minPassLength = 8;
+//  var maxPassLength = 128;
+ if(passLengthPrompt<8){
+   alert("Password needs atleast 8 characters")
+ }else if(passLengthPrompt>128){
+   alert("Password needs to be less than 128 characters")
+ }else{
+ var specialCharacterConfirm = confirm("Click OK to confirm using special characters");
+ var upperCaseConfirm = confirm("Click OK to confirm using upper case characters");
+ var lowerCaseConfirm = confirm("Click OK to confirm using lower case characters");
+}
 }
