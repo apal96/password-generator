aaa37b02dbf49cbb65028646d67189d94d84bf9c
apal <alishapal96@gmail.com>
2021-06-24 20:40:12 -0700

    add function userPassSelections

--- Develop/script.js
+++ Develop/script.js
@@ -23,3 +23,22 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function userPassSelctions(specialCharacter,upperCase,lowerCase){
+  var passLength=prompt("How many characters should the password contain?) ");
+  console.log(passLength)
+  if(passLength<8){
+   alert("Password needs atleast 8 characters")
+  }else if(passLength>128){
+   alert("Password needs to be less than 128 characters")
+  }else if(passLength>=8 && passLength<=128){
+    passOptions.specialCharConfirm= confirm("Click OK to confirm using special characters");
+    console.log(passOptions.specialCharConfirm)
+    passOptions.uppercaseConfirm = confirm("Click OK to confirm using upper case characters");
+    console.log(passOptions.uppercaseConfirm)
+    passOptions.lowercaseConfirm= confirm("Click OK to confirm using lower case characters");
+    console.log(passOptions.lowercaseConfirm)
+  }else{
+  alert("Please enter an integer between 8 and 128") 
+  }
+}
